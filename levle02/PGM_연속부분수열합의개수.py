b20e6d98cac70b8d70172ef189f9c39bb238891b
jiho4399 <j2h4399@gmail.com>
2023-03-05 16:52:11 +0900

    reset

--- levle02/PGM_연속부분수열합의개수.py
+++
@@ -1,9 +0,0 @@
-def solution(elements):
-    elements2 = elements * 2
-    s = set()
-
-    for i in range(len(elements)):
-        for j in range(len(elements)):
-            s.add(sum(elements2[j:j+i+1]))
-
-    return len(s)
